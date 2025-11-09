08a3083fb8b896ec4a82613a0833a5c16c1bc281
siddhant1729 <shaurya.sid1729@gmail.com>
2025-11-10 00:06:26 +0530

    Added run_all.sql automation script and updated README.md

---
+++ db/run_all.sql
@@ -0,0 +1,10 @@
+-- Create the database if not exists
+CREATE DATABASE IF NOT EXISTS dbmsproject;
+USE dbmsproject;
+
+-- Run all SQL files in order
+SOURCE C:/Users/shaur/OneDrive/Desktop/DBMS_PROJECT/db/createtables.sql;
+SOURCE C:/Users/shaur/OneDrive/Desktop/DBMS_PROJECT/db/insertdata.sql;
+SOURCE C:/Users/shaur/OneDrive/Desktop/DBMS_PROJECT/db/procedures&functions.sql;
+SOURCE C:/Users/shaur/OneDrive/Desktop/DBMS_PROJECT/db/triggers.sql;
+SOURCE C:/Users/shaur/OneDrive/Desktop/DBMS_PROJECT/db/views.sql;
